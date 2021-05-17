12f4367aecff8efe1bf91dcc26038d0c8cf8bd39
vikkie02 <abivictoria02@gmail.com>
2021-05-17 04:31:04 -0700

    vikkie first project

---
+++ vikkieBasics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vikkieBasics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
